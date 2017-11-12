417475d51df28882328c1d6d7ba9445115f2079a
unknown <tylersweeney@gmail.com>
2017-11-12 15:40:21 -0800

    add files and basic layout

---
+++ assets/javascript/script.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAPqQjpWxW2RD2Yaf1D_bsbP28wAxYuU9U",
+  authDomain: "train-time-9ec8e.firebaseapp.com",
+  databaseURL: "https://train-time-9ec8e.firebaseio.com",
+  projectId: "train-time-9ec8e",
+  storageBucket: "",
+  messagingSenderId: "97732188055"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.databse();
+
